92525004ab0a52e32bdd571d84d74cdec19df50f
dmtrung14 <trungdangminh14012004@gmail.com>
2021-11-01 11:47:58 +0700

    crime

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,12 @@
 var accordions = document.getElementsByClassName("accordion");
 for (var i = 0; i < accordions.length; i++) {
-  accordions[i].onclick = function() {
-    
-    this.classList.toggle('is-open');
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      // accordion is currently open, so close it
-      content.style.maxHeight = null;
-    } else {
-      // accordion is currently closed, so open it
-      content.style.maxHeight = content.scrollHeight + "px";
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
     }
-  }
-}

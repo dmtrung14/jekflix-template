9efe7c4f1a5a476232bfbe21062c5c00411a50ec
dmtrung14 <trungdangminh14012004@gmail.com>
2021-11-02 15:54:35 +0700

    lmao

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,9 +2,7 @@ var accordions = document.getElementsByClassName("accordion");
 
 for (var i = 0; i < accordions.length; i++) {
   accordions[i].onclick = function() {
-    this.classList.toggle('is-open');
-
-    var content = this.nextElementSibling;
+      var content = this.nextElementSibling;
     if (content.style.maxHeight) {
       // accordion is currently open, so close it
       content.style.maxHeight = null;
